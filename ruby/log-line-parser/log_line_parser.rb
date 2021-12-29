b717515488b6769b7f8b138f9dd7ab2f9e171c87
Thomas Betous <tbetous@users.noreply.github.com>
2021-12-29 14:17:41 +0100

    Fix log line parser

--- ruby/log-line-parser/log_line_parser.rb
+++ ruby/log-line-parser/log_line_parser.rb
@@ -1,17 +1,36 @@
 class LogLineParser
+  ERROR_TAG = 'ERROR'.freeze
+  WARNING_TAG = 'WARNING'.freeze
+  INFO_TAG = 'INFO'.freeze
+
+  LOG_REGEXPS = {
+    error: /^\[#{ERROR_TAG}\]:\s/,
+    warning: /^\[#{WARNING_TAG}\]:\s/,
+    info: /^\[#{INFO_TAG}\]:\s/
+  }.freeze
+
   def initialize(line)
     @line = line
   end
 
   def message
-    raise 'Please implement the LogLineParser#message method'
+    @line.sub(Regexp.union(LOG_REGEXPS.values), '').strip
   end
 
   def log_level
-    raise 'Please implement the LogLineParser#log_level method'
+    return 'error' if error?
+    return 'warning' if warning?
+
+    'info'
   end
 
   def reformat
-    raise 'Please implement the LogLineParser#reformat method'
+    "#{message} (#{log_level})"
+  end
+
+  LOG_REGEXPS.each do |type, regexp|
+    define_method("#{type}?") do
+      @line.match?(regexp)
+    end
   end
 end

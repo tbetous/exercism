30b1019ec408427427eb23a5f626d0e00a66f6bc
Thomas Betous <tbetous@users.noreply.github.com>
2021-12-30 15:59:17 +0100

    Improve word count

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,15 +1,9 @@
 class Phrase
   def initialize(phrase)
-    @phrase = phrase
-  end
-
-  def splitted_words
-    @phrase.downcase.gsub(/(\w'\w|\w)+/)
+    @words = phrase.downcase.gsub(/(\w'\w|\w)+/)
   end
 
   def word_count
-    @word_count ||= splitted_words.each_with_object({}) do |word, counts|
-      counts[word] = (counts[word] || 0) + 1
-    end
+    @word_count ||= @words.tally
   end
 end
